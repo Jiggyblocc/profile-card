14f1b31c5c31d85be67ddeace29bc311dfc2822b
User <user@Jiggybloccs-MacBook-Air.local>
2025-10-19 02:57:23 +0100

    Updated Javascript function to show the time in miliseconds

--- script.js
+++ script.js
@@ -2,24 +2,9 @@ document.addEventListener("DOMContentLoaded", () => {
   const timeElement = document.querySelector('[data-testid="test-user-time"]');
 
   function updateTime() {
-    const now = new Date();
-    const options = {
-      weekday: "long",  // e.g. Monday
-      year: "numeric",  // e.g. 2025
-      month: "long",    // e.g. October
-      day: "numeric",   // e.g. 18
-      hour: "2-digit",  // e.g. 08
-      minute: "2-digit",
-      second: "2-digit",
-    };
-
-    // Format and update
-    timeElement.textContent = now.toLocaleString("en-US", options);
+    timeElement.textContent = Date.now();
   }
 
-  // Run immediately
-  updateTime();
-
-  // Update every second
-  setInterval(updateTime, 1000);
+  updateTime();               // Set time once on page load
+  setInterval(updateTime, 1000); // Update every 1 second
 });
